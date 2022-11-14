5535305c8fb5ad7a4e5b62d0847858489468c249
Adeshc2k <117651499+Adeshc2k@users.noreply.github.com>
2022-11-14 13:30:15 +0530

    Calculator Project
    final calculator task

---
+++ myscript.js
@@ -0,0 +1,41 @@
+let display = document.getElementById("display");
+console.log(display);
+let buttons = Array.from(document.getElementsByClassName("button"));
+console.log(buttons);
+buttons.map(button => {
+  button.addEventListener('click',(e)=>{
+      console.log(e.target.innerText);
+    switch (e.target.innerText) {
+
+      case 'c':
+        display.innerText = '';
+        break;
+
+  
+    case "Del":
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0,-1);
+        }
+          break;
+    case '/':
+          console.log('indide /');
+              if (display.innerText) {
+                display.innerText = display.innerText.slice(0,-1);
+              }
+                break;
+          case '=':
+          try{
+            console.log(typeof(display.innerText));
+            console.log(display.innerText);
+            display.innerText = eval(display.innerText);
+          }catch{
+            display.innerText = 'Error!';
+          }
+          break;
+
+      default:
+      display.innerText += e.target.innerText;
+
+    }
+  });
+});
